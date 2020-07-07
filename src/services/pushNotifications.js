972cf247b594234e34e6ce27217f9c20550a2c06
Kavita Patel <patel16kavita@gmail.com>
2020-07-07 13:33:50 +0530

    configuration schanges for rnc push notification ios

--- src/services/pushNotifications.js
+++ src/services/pushNotifications.js
@@ -1,6 +1,7 @@
 import PushNotification from 'react-native-push-notification';
-import {PushNotificationIOS} from 'react-native';
-
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import {Platform} from 'react-native';
+import moment from 'moment';
 const configure = () => {
   PushNotification.configure({
     onRegister: function(token) {
@@ -25,21 +26,33 @@ const configure = () => {
 };
 
 const localNotification = () => {
-  PushNotification.localNotificationSchedule({
-    date: new Date(Date.now() + 60 * 1000),
-    autoCancel: true,
-    largeIcon: 'ic_launcher',
-    smallIcon: 'ic_notification',
-    color: 'green',
-    vibrate: true,
-    vibration: 300,
-    title: 'Devie',
-    message: 'Hey there is a message from devie !',
-    playSound: true,
-    soundName: 'default',
-    actions: '["Accept", "Reject"]',
-    repeatType: 'minute',
-  });
+  if (Platform.OS === 'ios') {
+    console.log('ios noe');
+    PushNotificationIOS.presentLocalNotification({
+      // fireDate: moment(new Date(Date.now() + 60 * 1000)).format(
+      //   'YYYY-MM-DDTHH:mm:ss.SSSZ',
+      // ),
+      alertTitle: 'Devie',
+      alertBody: 'Hey there is a message from devie !',
+      // repeatInterval: 'minute',
+    });
+  } else {
+    PushNotification.localNotificationSchedule({
+      date: new Date(Date.now() + 60 * 1000),
+      autoCancel: true,
+      largeIcon: 'ic_launcher',
+      smallIcon: 'ic_notification',
+      color: 'green',
+      vibrate: true,
+      vibration: 300,
+      title: 'Devie',
+      message: 'Hey there is a message from devie !',
+      playSound: true,
+      soundName: 'default',
+      actions: '["Accept", "Reject"]',
+      repeatType: 'minute',
+    });
+  }
 };
 
 export {configure, localNotification};
