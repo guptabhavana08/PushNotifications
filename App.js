7db4f5256343f2131beb096360c8b216de53e4f0
Bhavana Gupta <Bhavana>
2020-07-07 16:05:20 +0530

    App.js

--- App.js
+++ App.js
@@ -3,19 +3,20 @@ import {pushNotifications} from './src/services/Index';
 import AppContainer from './src/services/AppContainer';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
-useEffect(() => {
-  PushNotificationIOS.requestPermissions().then(
-    data => {
-      console.log('PushNotificationIOS.requestPermissions', data);
-    },
-    data => {
-      console.log('PushNotificationIOS.requestPermissions failed', data);
-    },
-  );
-}, []);
-
 pushNotifications.configure();
 
-const App = () => <AppContainer />;
+const App = () => {
+  useEffect(() => {
+    PushNotificationIOS.requestPermissions().then(
+      data => {
+        console.log('PushNotificationIOS.requestPermissions', data);
+      },
+      data => {
+        console.log('PushNotificationIOS.requestPermissions failed', data);
+      },
+    );
+  }, []);
+  return <AppContainer />;
+};
 
 export default App;
